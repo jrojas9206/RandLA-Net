02be95739b88aea86de665aa6e925e7853bb3887
Juan Pablo <jrojas9206@gmail.com>
2021-02-01 07:15:48 +0100

    update merge labels

--- utils/merge_label_apple.py
+++ utils/merge_label_apple.py
@@ -4,19 +4,7 @@ import argparse
 import numpy
 import glob
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Label Merging - RandLA-NET")
-    parser.add_argument("--inputDir_labels", type=str, help="Path to the predicted labels", default="./randlanet/predictions/")
-    parser.add_argument("--inputDir_pointc", type=str, help="Path to the point clouds", default="./data/realData/test/")
-    parser.add_argument("--outputDir", type=str, help="Path to write the new files", default="./output/")
-    args = parser.parse_args()
-    
-    input_dir = "/gpfswork/rech/wwk/uqr22pt/data_field/test/"
-    label_dir = "/gpfswork/rech/wwk/uqr22pt/model_RandLA-Net/test/Log_2020-06-18_12-53-43/predictions/"
-    output_dir = "/gpfswork/rech/wwk/uqr22pt/pred_RandLA-Net_field_HiRes"
-
-
+def merge_pointCloudAndLabels(input_dir, label_dir, output_dir):
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
@@ -40,3 +28,13 @@ if __name__ == "__main__":
         output_filename = os.path.join(output_dir, basename + '.txt')
         numpy.savetxt(output_filename, x)
         print("{}/{} : {}".format(i, len(filenames), output_filename))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Label Merging - RandLA-NET")
+    parser.add_argument("--inputDir_labels", type=str, help="Path to the predicted labels", default="./randlanet/predictions/")
+    parser.add_argument("--inputDir_pointc", type=str, help="Path to the point clouds", default="./data/realData/test/")
+    parser.add_argument("--outputDir", type=str, help="Path to write the new files", default="./output/")
+    args = parser.parse_args()
+
+    merge_pointCloudAndLabels(args.inputDir_pointc, args.inputDir_labels, args.outputDir)
+
