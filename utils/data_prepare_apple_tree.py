0b75f9781f53b86610c64884572f6cd2849b2a96
Juan Pablo <jrojas9206@gmail.com>
2021-02-09 07:19:50 +0100

    update data prepare apple

--- utils/data_prepare_apple_tree.py
+++ utils/data_prepare_apple_tree.py
@@ -98,14 +98,14 @@ def convert_for_training(filename, num_fold, output_dir, grid_size=0.001, protoc
     if protocol == "synthetic":
         # TODO : hack must be remove
         colors = numpy.zeros((data.shape[0], 3), dtype=numpy.uint8)
-        labels = data[:, 3].astype(numpy.uint8)
+        labels = data[:, -1].astype(numpy.uint8)
     elif protocol == "field_only_xyz":
         colors = numpy.zeros((data.shape[0], 3), dtype=numpy.uint8)
-        labels = data[:, 6].astype(numpy.uint8)
+        labels = data[:, -1].astype(numpy.uint8)
     elif protocol == "field":
-        adr = normalize(data[:, 3:6]) * 255
+        adr = normalize(data[:, 3:-1]) * 255
         colors = adr.astype(numpy.uint8)
-        labels = data[:, 6].astype(numpy.uint8)
+        labels = data[:, -1].astype(numpy.uint8)
     else:
         exit("unknown protocol")
 
